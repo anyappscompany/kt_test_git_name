86b602ca6069a405a255f70bff0f02c73bf2bfa7
yurii.kaskov <yurii.kaskov@jelvix.com>
2021-06-05 10:43:36 +0300

    Initial commit

---
+++ app/src/main/java/ua/com/anyapps/kt_test_git_name/MainActivity.kt
@@ -0,0 +1,11 @@
+package ua.com.anyapps.kt_test_git_name
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
